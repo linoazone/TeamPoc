9bc4faaf02ef6f2045ad02228b3ac8be7cbc2603
marisotto <50438700+marisotto@users.noreply.github.com>
2020-01-04 17:20:44 +0100

    Add files via upload

---
+++ DHT_LCD.ino
@@ -0,0 +1,96 @@
+#include<LiquidCrystal.h>
+#include <DHT.h>
+
+int DHT11pin=A0;
+//Initialisation de l'écran LCD avec les numéros des pins utilisés
+LiquidCrystal LCD(12, 11, 4, 3, 2, 1); 
+
+int Heating=5;
+int Cooling=6;
+int MoreMoist=7;
+int MoreDry=8;
+float DHTTemp=0; // temp mesurée
+float DHTHum=0; // pourcentage humidité
+
+dht DHT ; // initialisation du capteur
+
+void setup() {
+  Serial.begin(9600);
+  LCD.begin(16,2); // nb de colonnes et lignes de l'ecran
+  LCD.clear();
+  
+  pinMode(Heating, OUTPUT);
+  pinMode(Cooling, OUTPUT);
+  pinMode(MoreMoist, OUTPUT);
+  pinMode(MoreDry, OUTPUT);
+  pinMode(DHT11pin, INPUT);
+  delay(1000);
+}
+
+void loop() {
+  int LDRvalue = analogRead(LDR);
+  
+   DHT.read11(DHT11pin);
+   DHTTemp=(DHT.temperature);
+   DHTHum=(DHT.humidity);
+
+  // PARTIE LCD
+  if(isnan(DHTTemp) || isnan(DHTHum)) // si les valeurs retournées ne sont pas des nb
+  {
+    LCD.setCursor(0, 1); //Positionnement du curseur
+    LCD.print(DHTTYPE); //On affiche le type de capteur
+    LCD.setCursor(5, 1);
+    LCD.print(" illisible"); //On affiche l'erreur
+  }
+  else
+  {
+    //mise en forme et affichage des informations sur l'écran LCD
+    LCD.setCursor(0, 0); //Positionnement du curseur
+    LCD.print("Degres : ");
+    LCD.setCursor(9, 0);
+    LCD.print(DHTTemp); //Affichage de la température
+    LCD.setCursor(13, 0);
+    LCD.print((char)223); //Affiche le caractère ° (degrés)
+    LCD.setCursor(14, 0);
+    LCD.print("C"); //En degrés Celsuis
+    LCD.setCursor(0, 1);
+    LCD.print("Humidite : ");
+    LCD.setCursor(11, 1);
+    LCD.print(DHTHum); //Affichage de l'humidité
+    LCD.setCursor(15, 1);
+    LCD.print("%");
+  }
+  delay(5000);
+
+  
+  // PARTIE DHT11
+   if (DHTTemp < 16) {
+    digitalWrite(Heating, HIGH);
+    delay (1000);
+    digitalWrite(Cooling, LOW);
+    delay (1000);
+   }
+
+   if (DHTTemp > 22) {
+    digitalWrite(Heating, LOW);
+    delay (1000);
+    digitalWrite(Cooling, HIGH);
+    delay (1000);
+   }
+
+   if (DHTHum < 41) {
+    digitalWrite(MoreMoist, HIGH);
+    delay (1000);
+    digitalWrite(MoreDry, LOW);
+    delay (1000);
+   }
+
+   if (DHTTemp > 80) {
+    digitalWrite(MoreMoist, LOW);
+    delay (1000);
+    digitalWrite(MoreDry, HIGH);
+    delay (1000);
+   }
+
+   delay (500);   
+}
